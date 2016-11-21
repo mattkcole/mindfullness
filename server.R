4cef811b4bbc5ef6a390f0649963c11aa3b7eb0a
mattkcole <mattcole1093@gmail.com>
2016-11-21 12:41:27 -0500

    added tabs, updated gitign

--- server.R
+++ server.R
@@ -7,7 +7,7 @@
 
 library(shiny)
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
   output$distPlot <- renderPlot({
 
@@ -19,5 +19,27 @@ shinyServer(function(input, output) {
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
 
   })
+  
+        user <- reactive({
+                session$user
+        })
+        
+        isResearcher <- reactive({
+                if (user() == "researcher"){
+                        return(TRUE)
+                } else{
+                        return(FALSE)
+                }
+        })
+        
+        myData <- reactive({
+                if (isResearcher()){
+                        # If a manager, show everything.
+                        return(RecData)
+                } else{
+                        # If a regular salesperson, only show their own sales.
+                        return(RecData[RecData$person == user(),])
+                }
+        })
 
 })
--- ui.R
+++ ui.R
@@ -10,24 +10,40 @@ library("tuneR")
 library("markdown")
 
 shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot"),
-      tags$audio(src = "nordish.mp3", type = "audio/mp3", autoplay = NA, controls = NA)
-    )
-  )
+        
+        # Application title
+        titlePanel("Be mindfull"),
+        
+        # Sidebar with a slider input for number of bins
+        navlistPanel(
+                tabPanel("Well Wishes",
+                         mainPanel("We wish you well :)", 
+                                   fluidRow(
+                                   tags$audio(src = "NICU_5_mins_Well_wishes.mp3", 
+                                              type = "audio/mp3", 
+                                              controls = T)),
+                                   fluidRow(tags$audio(src = "NICU_10min_Well_wishes.mp3", 
+                                              type = "audio/mp3", 
+                                              controls = T)))),
+                tabPanel("Nordish By Nature",
+                         mainPanel("hey this should say the tab title: tab 2", 
+                                   tags$audio(src = "nordish.mp3", 
+                                              type = "audio/mp3", 
+                                               controls = NA))),
+                
+                tabPanel("tab 3",
+                         mainPanel("hey this should say the tab title: tab 3", 
+                                   tags$audio(src = "nordish.mp3", 
+                                              type = "audio/mp3", 
+                                              controls = NA))),
+                
+                tabPanel("tab 4",
+                         mainPanel("hey this should say the tab title: tab 4", 
+                                   tags$audio(src = "nordish.mp3", 
+                                              type = "audio/mp3", 
+                                              controls = NA)))
+                
+                
+        )
 ))
+
